bdb1f70d7677de676e8c8b9448abe17bb98e280e
NikaMonroes <monicaromerofreelance@gmail.com>
2025-07-01 09:51:30 -0500

    Update footer.php
    wp

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,6 @@
 
 <footer class="bg-primary text-white text-center py-3 fixed-bottom">
     <p class="mb-0">Proyecto desarrollado por <strong>MR Freelance</strong> | 2025</p>
-    <p><a href="https://wa.me/573026838855/?text=Hola, me interesa conocer sus servicios de diseño." 
+    <p><a href="https://wa.me/573026XXXXX/?text=Hola, me interesa conocer sus servicios de diseño." 
           class="text-white fw-bold" target="_blank">Contáctame en WhatsApp 🚀</a></p>
 </footer>
